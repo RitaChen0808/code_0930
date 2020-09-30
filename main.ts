5f165976083214948033c41f37192befcf4b3647
RitaChen0808 <71751332+RitaChen0808@users.noreply.github.com>
2020-09-30 15:21:22 +0800

    刪除

--- main.ts
+++ main.ts
@@ -7,23 +7,5 @@ basic.forever(function () {
         basic.showString("A")
     } else if (scores < 80) {
         basic.showString("A-")
-    } else if (scores < 77) {
-        basic.showString("B+")
-    } else if (scores < 73) {
-        basic.showString("B")
-    } else if (scores < 70) {
-        basic.showString("B-")
-    } else if (scores < 67) {
-        basic.showString("C+")
-    } else if (false) {
-        basic.showString("C")
-    } else if (scores < 63) {
-        basic.showString("C-")
-    } else if (scores < 60) {
-        basic.showString("Hello!")
-    } else if (scores < 50) {
-        basic.showString("Hello!")
-    } else {
-        basic.showString("Hello!")
     }
 })
